255746ddb4771320f3b571af06e75817aa95a8b9
unknown <hernan97carp@hotmail.com>
2024-02-15 20:59:51 -0300

    SPAC-1 test(Account):All tests have passed and the feature is working as expected

--- cypress/e2e/Tests/Suites/OrangeHRM/Account.Login.Logout.cy.js
+++ cypress/e2e/Tests/Suites/OrangeHRM/Account.Login.Logout.cy.js
@@ -12,14 +12,14 @@ describe("SpaceBeyond | Account | Log-IN and Log-Out", () => {
 
     it("US-GX-02 | TC2: Validar No poder iniciar sesion cuando el campo name esta Vacio", () => {
         cy.loginSpace("", "123456aB")
-        //validate login:
+
         cy.contains("Name is a required field.").should("be.visible")
         cy.contains("Password is a required field.").should("not.exist")
     });
 
         it("US-GX-03 | TC3: Validar No poder iniciar sesion cuando el campo password esta Vacio", () => {
         cy.loginSpace("Hernan", "")
-        //validate login:
+   
      
             cy.contains("Password is a required field.").should("be.visible")
               cy.contains("Name is a required field.").should("not.exist")
@@ -28,7 +28,7 @@ describe("SpaceBeyond | Account | Log-IN and Log-Out", () => {
 
         it("US-GX-04 | TC4: Validar Salir de sesion exitosamente luego haber iniciado sesion", () => {
         cy.loginSpace("Hernan", "123456aB")
-        //validate login:
+ 
      
             cy.get(".mui-dropdown button").click()
             cy.get(".mui--is-open a").click()
